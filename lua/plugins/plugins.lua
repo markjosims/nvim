66a31ef25a4a41d34475f2efcc383c87d09e028b
Mark J Simmons <mjsimmons@ucsd.edu>
2026-04-06 11:17:34 -0700

    initial commit

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "jalvesaq/zotcite",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("zotcite").setup({
+        -- your options here (see doc/zotcite.txt)
+      })
+    end,
+  },
+
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    lazy = false,
+    opts = {},
+  },
+}
