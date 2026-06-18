51c7d36a94f4ba29c0f3846418ee5fa203fc4be2
Mark Joseph Simmons <mark.simmons@cresta.ai>
2026-06-18 22:38:16 +0000

    change color theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "ntk148v/habamax.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "habamax.nvim",
+    },
+  },
+}
+
